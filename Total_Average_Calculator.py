4387159e7e77178ba55814961e0700cc8e828e85
LKim96 <47342278+LKim96@users.noreply.github.com>
2023-06-04 12:00:55 -0700

    Add files via upload

---
+++ Total_Average_Calculator.py
@@ -0,0 +1,29 @@
+total = 0.0
+count = 0
+average = 0.0
+
+while True:
+    x = input("Enter a number: ")
+
+    try:
+        inp = float(x)
+    except:
+        if x == 'done':
+            break
+        else:
+            print('Invalid Input')
+            continue
+
+    total = total + inp
+    count += 1
+
+if average != 0.0:
+    average = format(total/count, ".2f")
+else:
+    average = 0.0
+
+
+print("\n")
+print("The total is: ", total)
+print("The amount of numbers entered: ", count)
+print("The average is: ", average)
